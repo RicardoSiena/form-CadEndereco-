f295ed2bada6d614aae0719ff68eb7b0a3633954
Ricardo Siena <ricardo.siena@alunoescolassociais.org.br>
2024-09-20 11:22:26 -0300

    Adiciona código JS que remove o que foi inserido nos inputs e verifica se os caractéres inseridos no campo de CEP são números

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'; //ativa o modo restrito
+// Este modo faz com que o javascript opera de forma mais segura e rigorosa, ajudando a evitar erros comuns de programação.
+/* consumo de API - https://viacep.com.br/ */
+ 
+//função para limpar campos preenchidos
+
+const limparFormulario = () => {
+    document.getElementById ('logradouro').value = '';
+    document.getElementById ('bairro').value = '';
+    document.getElementById ('localidade').value = '';
+    document.getElementById ('uf').value = '';
+    document.getElementById ('complemento').value = '';
+    document.getElementById ('numero').value = '';
+}
+
+// Verifica se o CEP é válido
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
